b7d47f3d47a2872221b23a4314d6b1d804e4252a
atienzisaa <65943068+atienzisaa@users.noreply.github.com>
2020-06-10 15:56:25 +1200

    atienzisaa Core V1

--- robot.cpp
+++ robot.cpp
@@ -1,109 +1,63 @@
 #include "robot.hpp"
+#include <numeric>
+#include <vector>
+using namespace std;
 
-int pixel(){
-	int whiteCount = 0;
-	int whitePositions = 0;
-	float whiteAverage = 0;
-	
-	int leftCount = 0;
-	int rightCount = 0;
-	
-	int* pixelArray = new int[cameraView.width];
-	int* lostLeft = new int[cameraView.height];
-	int* lostRight = new int[cameraView.height];
-	
-	for(int i = 0; i < cameraView.width; i++){
-		pixelArray[i] = get_pixel(cameraView, (cameraView.height/4), i, 3);
-		if(pixelArray[i] == 255){
-			pixelArray[i] = 1;
-			whiteCount = whiteCount + 1;
-		}
-		else{
-			pixelArray[i] = 0;
-		}
-		
-		
-		if(pixelArray[i] == 1){
-			whitePositions = whitePositions + i;
-			std::cout<<whitePositions<<std::endl;
-		}
+//sets the speed of the motors depending on the passed arguments		issue: shaking robot due to oscillation-like change in left and right. Fix could be smoother turns and implementation of 'error'
+int speed(int left, int right){
+	double vLeft = 20.0;
+    double vRight = 20.0;
+    double toTurn = 15.0;
+    double sharpTurn = 10.0;
+    
+	if(left > right){
+		setMotors(toTurn,vLeft); 
 	}
-	
-	for(int i = 0; i < cameraView.height; i++){
-		lostLeft[i] = get_pixel(cameraView, i, (cameraView.width/4), 3);
-		lostRight[i] = get_pixel(cameraView, i, ((cameraView.width/4) * 3), 3);
-		if(lostLeft[i] == 255){
-			leftCount = leftCount + 1;
-		}
-		if(lostRight[i] == 255){
-			rightCount = rightCount + 1;
-		}
+	else if(right > left){
+		setMotors(vRight,toTurn); 
 	}
-	
-	if(!(whitePositions == 0 || whiteCount == 0)){
-		whiteAverage = (whitePositions / whiteCount);
+	else if(left > right*2){
+		setMotors(sharpTurn,vLeft);
 	}
-	if(whitePositions == 0 || whiteCount == 0){
-		if(leftCount >= rightCount){
-			return(4);
-		}
-		else{
-			return(3);
-		}
+	else if(right > left*2){
+		setMotors(vRight,sharpTurn); 
 	}
-			
-	if(whiteAverage > (cameraView.width/2)){
-		return(1);
+	else{
+		setMotors(vLeft,vRight);
 	}
-	if(whiteAverage < (cameraView.width/2)){
-		return(2);
-	}
-	if(whiteAverage > (cameraView.width/3)){
-		return(3);
-	}
-	if(whiteAverage < (cameraView.width/3)){
-		return(4);
-	}
-	
-	return(0);
+	return 0;
 }
 
 int main(){
 	if (initClientRobot() !=0){
 		std::cout<<" Error initializing robot"<<std::endl;
 	}
-    double vLeft = 10.0;
-    double vRight = 10.0;
     while(1){
 		takePicture();
-		//SavePPMFile("i0.ppm",cameraView);
-		pixel();
-		if(pixel() == 1){
-			vLeft = 12.0;
-			vRight = 10.0;
+		std::vector<int> yVect;
+		for ( int column =0 ; column < cameraView.width ; column++){	
+			double white = get_pixel(cameraView, (cameraView.height/2),column,3);			//gets luminosity of cameraView.height/2 vs column
+			if(white==255){
+				yVect.push_back(1);						//adds 1 to vector if there is a white pixel
+			}
+			else{
+				yVect.push_back(0);						//adds 0 to vector if there isn't
+			}
+	    }
+	    int left = std::accumulate(yVect.begin(), yVect.begin()+int(yVect.size()/2), 0);				//finds sum of first half
+	    int right = std::accumulate(yVect.begin()+int(yVect.size()/2), yVect.end(), 0);					//finds sum of last half
+	    
+	    //std::cout<<"left"<<left<<endl;
+	    //std::cout<<"right"<<right<<endl;
+	    
+	    for (int i = 0; i < yVect.size(); i++){
+			std::cout<<yVect.at(i)<<' ';
 		}
-		if(pixel() == 2){
-			vLeft = 10.0;
-			vRight = 12.0;
-		}
-		if(pixel() == 3){
-			vLeft = 14.0;
-			vRight = 10.0;
-		}
-		if(pixel() == 4){
-			vLeft = 10.0;
-			vRight = 14.0;
-		}
-		if(pixel() == 0){
-			vLeft = 10.0;
-			vRight = 10.0;
-		}
-		setMotors(vLeft,vRight);   
-		//std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
-		usleep(10000);
+		
+		speed(left, right);				//setting speed
+		usleep(10000);					//deletion of this could remove the shaking problem
   } //while
-
+	
 } // main
-
 
 
