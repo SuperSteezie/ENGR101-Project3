38acd30a3ca7914ef80e9424b3e1b016d16c00f3
atienzisaa <65943068+atienzisaa@users.noreply.github.com>
2020-06-16 00:38:00 +1200

    atienzisaa COMPLETION
    Issue: camera is asymmetric therefore, renders me unable to keep the robot on the middle of the white line once its moved. Robot thinks that if line is on right side its fine since this is default. (4 pixels on right and 0 on left is default).
    Fix: changing the robot.hpp file and moving the width of the camera by 2 pixels to the left to balance it out and make it symmetrical if possible.

--- robot.cpp
+++ robot.cpp
@@ -3,29 +3,35 @@
 #include <vector>
 using namespace std;
 
-//sets the speed of the motors depending on the passed arguments		issue: shaking robot due to oscillation-like change in left and right. Fix could be smoother turns and implementation of 'error'
-int speed(int left, int right){
-	double vLeft = 20.0;
-    double vRight = 20.0;
-    double toTurn = 15.0;
-    double sharpTurn = 10.0;
+/*	sets the speed of the motors depending on the passed arguments		
+ * 	issue: since the camera is asymmetric and default is 4 white pixels on the right, hard to make it so that when robot is on line it should not move.
+ *  Instead what happens is that if the white line is on the robot's right it thinks that everything's fine.
+ */
+int speed(int left, int right, vector<int> vector){
+	double straight = 20.0;				//default speed
+    double error;
     
-	if(left > right){
-		setMotors(toTurn,vLeft); 
+    if(right == 4){						//for when left and right is equal
+		error = 0;
+	}
+	else if(left > right){				//need != 2 condition since left is initalized with 2
+		error = left;					//left being the left half of the cam (from robot POV)
 	}
 	else if(right > left){
-		setMotors(vRight,toTurn); 
+		error = right;					//right being the left half of the cam (from robot POV)
 	}
-	else if(left > right*2){
-		setMotors(sharpTurn,vLeft);
+	//everything from here onwards is from Lecture 15 notes (kp gained via trial and error)
+	double dv = 0.8 * error;
+	double vRight = straight + dv;
+	double vLeft = straight - dv;
+	
+	if(left > right){
+		setMotors(vLeft, vRight);
 	}
-	else if(right > left*2){
-		setMotors(vRight,sharpTurn); 
+	else if(right > left){
+		setMotors(vRight, vLeft);
 	}
-	else{
-		setMotors(vLeft,vRight);
-	}
-	return 0;
+	return 0; 
 }
 
 int main(){
@@ -36,7 +42,7 @@ int main(){
 		takePicture();
 		std::vector<int> yVect;
 		for ( int column =0 ; column < cameraView.width ; column++){	
-			double white = get_pixel(cameraView, (cameraView.height/2),column,3);			//gets luminosity of cameraView.height/2 vs column
+			double white = get_pixel(cameraView,99,column,3);				//gets luminosity of basically the bottom of the camera vs column
 			if(white==255){
 				yVect.push_back(1);						//adds 1 to vector if there is a white pixel
 			}
@@ -44,17 +50,25 @@ int main(){
 				yVect.push_back(0);						//adds 0 to vector if there isn't
 			}
 	    }
-	    int left = std::accumulate(yVect.begin(), yVect.begin()+int(yVect.size()/2), 0);				//finds sum of first half
-	    int right = std::accumulate(yVect.begin()+int(yVect.size()/2), yVect.end(), 0);					//finds sum of last half
-	    
-	    //std::cout<<"left"<<left<<endl;
-	    //std::cout<<"right"<<right<<endl;
+	    int left = 0;
+	    int right = 0;
+	    for(double i = 0; i < yVect.size(); i++){			//summing of values. Top half goes into left, etc
+			if (i < (yVect.size()/2)){
+				left += yVect[i];
+			}
+			else{
+				right += yVect[i];
+			}
+		}
+		//cout of vector halves and each element in vector
+	    std::cout<<"left"<<left<<endl;
+	    std::cout<<"right"<<right<<endl;
 	    
 	    for (int i = 0; i < yVect.size(); i++){
 			std::cout<<yVect.at(i)<<' ';
 		}
 		
-		speed(left, right);				//setting speed
+		speed(left, right, yVect);				//setting speed
 		usleep(10000);					//deletion of this could remove the shaking problem
   } //while
 	
