2db2ddcd123f581d776434b5015e57f97ed63317
hanrahrhys <66288365+hanrahrhys@users.noreply.github.com>
2020-06-16 11:53:54 +1200

    Challenge idea v1
    Added a bit of code to make the robot switch to challenge code for the challenge maze then slightly follow the left wall.

--- robot.completion.cpp
+++ robot.completion.cpp
@@ -11,10 +11,10 @@ int horizontalPixel(){
 	int whitePositions = 0;
 	float whiteAverage = 0;
 	
-	int* pixelArray = new int[cameraView.width]; //Creates pixel array
+	int* pixelArray = new int[cameraView.width];
 	
 	for(int i = 0; i < cameraView.width; i++){ //Runs through a line of pixels
-		pixelArray[i] = get_pixel(cameraView, (cameraView.height/4), i, 3); //Builds an array of pixels on a line
+		pixelArray[i] = get_pixel(cameraView, (30), i, 3); //Builds an array of pixels on a line
 		if(pixelArray[i] == 255){
 			pixelArray[i] = 1; //Sets all white pixels to 1
 			whiteCount = whiteCount + 1;
@@ -50,16 +50,10 @@ int horizontalPixel(){
 	return(0); //If the white line is in the center, move straight
 }
 
-/**
- * Right side pixel function
- * Takes data from camera image and builds a 1D array of the pixel values on a line
- * The line is on the right side of the image, used to tell the robot when to turn right when there is a sharp turn
- */
-
-int rightSidePixel(){
+int cornerPixel(){
 	int whiteCount = 0;
 	
-	int* pixelArray = new int[cameraView.height]; //Creates pixel array
+	int* pixelArray = new int[cameraView.height];
 	
 	for(int i = 0; i < cameraView.height; i++){ //Runs through a line of pixels
 		pixelArray[i] = get_pixel(cameraView, i, (cameraView.width - 10), 3); //Builds an array of pixels on a line
@@ -72,11 +66,70 @@ int rightSidePixel(){
 		}
 	}
 	
-	if(whiteCount > 2){ //If there are white pixels in the array
-		return(1); //Turn the robot right sharply
+	if(whiteCount > 2){
+		return(1);
 	}
 	
-	return(0); //Otherwise move straight
+	return(0);
+}
+
+int challengeMaze(){
+	int redCount1 = 0;
+	int redCount2 = 0;
+	int isRed = 0;
+	double vLeft = 0;
+	double vRight = 0;
+	takePicture();
+	for(int i = 0; i < cameraView.height; i++){ 
+		isRed = get_pixel(cameraView, i, 60, 0); 
+		if(isRed == 255){
+			redCount1 = 1;
+		}
+		isRed = get_pixel(cameraView, i, 90, 0); 
+		if(isRed == 255){
+			redCount2 = 1;
+		}
+	}
+	if(redCount1 == 1 && redCount2 == 1){
+		for(int i = 0; i < 10; i++){
+			vLeft = 16.7;
+			vRight = -16.7;
+			setMotors(vLeft,vRight);
+		}
+	}else if(redCount1 == 0 && redCount2 == 0){
+		vLeft = 400;
+		vRight = 400;
+		setMotors(vLeft,vRight);
+		for(int i = 0; i < 10; i++){
+			vLeft = -16.7;
+			vRight = 16.7;
+			setMotors(vLeft,vRight);
+		}
+	}else if(redCount1 == 0 && redCount2 == 1){
+		for(int i = 0; i < 10; i++){
+			vLeft = -19;
+			vRight = 19;
+			setMotors(vLeft,vRight);
+		}
+		vLeft = 200;
+		vRight = 200;
+		setMotors(vLeft,vRight);
+	}else if(redCount1 == 1 && redCount2 == 0){
+		for(int i = 0; i < 10; i++){
+			vLeft = 17;
+			vRight = -17;
+			setMotors(vLeft,vRight);
+		}
+		vLeft = 200;
+		vRight = 200;
+		setMotors(vLeft,vRight);
+		for(int i = 0; i < 10; i++){
+			vLeft = -16;
+			vRight = 16;
+			setMotors(vLeft,vRight);
+		}
+	}
+	
 }
 
 int main(){
@@ -85,40 +138,57 @@ int main(){
 	}
     double vLeft = 10.0;
     double vRight = 10.0;
+    int challenge = 0;
+    takePicture();
+    int* pixelArray = new int[cameraView.height];
+    for(int i = 0; i < cameraView.height; i++){ //Runs through a line of pixels
+	pixelArray[i] = get_pixel(cameraView, i, (cameraView.width), 0); //Builds an array of pixels on a line
+	if(pixelArray[i] == 255){
+			challenge = 1;
+		}
+	}
+	vLeft = 1200;
+	vRight = 1200;
+	setMotors(vLeft,vRight);
     while(1){
-		takePicture();
-		if(rightSidePixel() == 1){
-			vLeft = 60.0;
-			vRight = 10.0;
+		if(challenge == 0){
+			takePicture();
+			//SavePPMFile("i0.ppm",cameraView);
+			if(cornerPixel() == 1){
+				vLeft = 40.0;
+				vRight = 10.0;
+				std::cout<<"hello"<<std::endl;
+			}
+			else if(horizontalPixel() == 1){
+				vLeft = 12.0;
+				vRight = 10.0;
+			}
+			else if(horizontalPixel() == 2){
+				vLeft = 10.0;
+				vRight = 12.0;
+			}
+			else if(horizontalPixel() == 3){
+				vLeft = 20.0;
+				vRight = 10.0;
+			}
+			else if(horizontalPixel() == 4){
+				vLeft = 10.0;
+				vRight = 20.0;
+			}
+			else if(horizontalPixel() == 5){
+				vLeft = -40.0;
+				vRight = 40.0;
+			}
+			else if(horizontalPixel() == 0){
+				vLeft = 10.0;
+				vRight = 10.0;
+			}
+			setMotors(vLeft,vRight);
+		}else if(challenge == 1){
+			challengeMaze();
 		}
-		else if(horizontalPixel() == 1){
-			vLeft = 12.0;
-			vRight = 10.0;
-		}
-		else if(horizontalPixel() == 2){
-			vLeft = 10.0;
-			vRight = 12.0;
-		}
-		else if(horizontalPixel() == 3){
-			vLeft = 20.0;
-			vRight = 10.0;
-		}
-		else if(horizontalPixel() == 4){
-			vLeft = 10.0;
-			vRight = 20.0;
-		}
-		else if(horizontalPixel() == 5){
-			vLeft = -40.0;
-			vRight = 40.0;
-		}
-		else if(horizontalPixel() == 0){
-			vLeft = 10.0;
-			vRight = 10.0;
-		}
-		setMotors(vLeft,vRight);
 		//std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
 		usleep(10000);
   } //while
 
 } // main
-
