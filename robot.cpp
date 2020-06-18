2227b1d5eb24c3d18fc0b7387cdad7a9283cbc04
hanrahrhys <66288365+hanrahrhys@users.noreply.github.com>
2020-06-18 12:07:01 +1200

    Update robot.cpp
    Small for loop to allow robot to get to the flag

--- robot.cpp
+++ robot.cpp
@@ -87,6 +87,8 @@ int main(){
 	}
     double vLeft = 10.0;
     double vRight = 10.0;
+    int leftCount = 0;
+    int testCount = 5;
     while(1){
 		takePicture();
 		/*if(rightSidePixel() == 1){
@@ -96,6 +98,7 @@ int main(){
 		if(leftSidePixel() == 1){
 			vLeft = 19.0;
 			vRight = 12.0;
+			leftCount = 0;
 		}
 		/*else if(horizontalPixel() == 1){
 			vLeft = 20.0;
@@ -107,8 +110,23 @@ int main(){
 			vRight = 40.0;
 		}*/
 		else if(leftSidePixel() == 0){
-			vLeft = 12.0;
-			vRight = 15.0;
+			vLeft = 11;
+			vRight = 15;
+			leftCount += 1;
+			if(leftCount > testCount && testCount > 1){
+				for(int i = 0; i < 120; i++){
+						vLeft = 10;
+						vRight = 15;
+						setMotors(vLeft,vRight);
+				}
+				for(int i = 0; i < 30; i++){
+						vLeft = 15;
+						vRight = 10;
+						setMotors(vLeft,vRight);
+				}
+				leftCount = 0;
+				testCount = 0;
+			}
 		}
 		/*else{
 			vLeft = 10.0;
